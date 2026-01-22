d54aa1038d1b86bdc00382be67785bcfd666a7f0
Prasaderp <117333300+Prasaderp@users.noreply.github.com>
2026-01-22 22:08:57 +0530

    2053 excel fix

--- src/schemes/s2053/subs/s20530019/ui_post_expenses.py
+++ src/schemes/s2053/subs/s20530019/ui_post_expenses.py
@@ -20,8 +20,7 @@ from src.utils_fiscal_year import get_fiscal_year_from_request
 from src.utils_scheme import get_scheme_from_cookies
 from src.utils_cache import ttl_cache
 from src.utils_timing import check_data_filling_allowed
-# TODO: Implement Excel export when template is ready
-# from src.excel_template_export import export_original_workbook
+from .excel_export import export_original_workbook_async
 from src.audit_service import AuditService
 from src.schemes.common.utils import build_post_expenses_district_sync_update
 from .models import PostExpenses
@@ -814,17 +813,17 @@ async def export_post_expenses_original(
     db: Session = Depends(get_db),
     district: Optional[str] = Query(None)
 ):
+    """Export original Excel workbook with production-grade throttling."""
     auth_level = request.cookies.get('auth_level')
     auth_unit = get_auth_unit(request)
+    fiscal_year = get_fiscal_year_from_request(request, db)
     user_district = None
     if auth_level == 'district':
         user_district = auth_unit
     elif auth_level in ('dco', 'officer1', 'officer2') and district:
         user_district = district
     _, sub_scheme = get_scheme_from_cookies(request)
-    # TODO: Implement Excel export when template is ready
-    raise HTTPException(status_code=501, detail="Excel export not yet implemented for this subscheme")
-    # return export_original_workbook(db, user_district=user_district, sub_scheme_code=sub_scheme)
+    return await export_original_workbook_async(db, user_district=user_district, sub_scheme_code=sub_scheme, fiscal_year=fiscal_year)
 
 @router.get("/export-sheet-only", response_class=StreamingResponse)
 async def export_post_expenses_sheet_only(
@@ -832,14 +831,14 @@ async def export_post_expenses_sheet_only(
     db: Session = Depends(get_db),
     district: Optional[str] = Query(None)
 ):
+    """Export only post expenses sheet with throttling."""
     auth_level = request.cookies.get('auth_level')
     auth_unit = get_auth_unit(request)
+    fiscal_year = get_fiscal_year_from_request(request, db)
     user_district = None
     if auth_level == 'district':
         user_district = auth_unit
     elif auth_level in ('dco', 'officer1', 'officer2') and district:
         user_district = district
     _, sub_scheme = get_scheme_from_cookies(request)
-    # TODO: Implement Excel export when template is ready
-    raise HTTPException(status_code=501, detail="Excel export not yet implemented for this subscheme")
-    # return export_original_workbook(db, only_sheet="post_expenses", user_district=user_district, sub_scheme_code=sub_scheme)
+    return await export_original_workbook_async(db, only_sheet="post_expenses", user_district=user_district, sub_scheme_code=sub_scheme, fiscal_year=fiscal_year)
